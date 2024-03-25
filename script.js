010ea970a9afc6236eababe306cc8c3052c16b30
Maureen Martin Onetti <maureen@acchiardo.fr>
2024-03-25 10:56:51 +0100

    ccs

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function toto(alcool) {
+function recupereliste(alcool) {
   fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + alcool) //fetch = chercher / ici on va chercher la donnée
     .then((response) => response.json()) //on va chercher la réponse et on le transforme en json
     .then((data) => {
       const { drinks } = data;
 
+      document.getElementById("cocktail").innerHTML= ''
+
+
       drinks.forEach((cocktail) => {
         //for each va chercher pour tout lister (boucle)
         const alcoolDiv = document.createElement("div");
-
-        //   alcoolDiv.classList.add("alcool");
-
+        alcoolDiv.classList.add("card");
         const nomCocktail = cocktail.strDrink;
         const imageCocktail = cocktail.strDrinkThumb;
-
         const ingredientsCocktail = [];
         for (let i = 0; i <= 14; i++) {
           const maCle = "strIngredient" + (i + 1);
@@ -21,7 +21,6 @@ function toto(alcool) {
             ingredientsCocktail.push(unIngredient);
           }
         }
-
         const mesurecocktail = [];
         for (let i = 0; i <= 14; i++) {
           const maCle = "strMeasure" + (i + 1);
@@ -31,25 +30,31 @@ function toto(alcool) {
           }
         }
 
-        let contenuDeMaDiv = "<h1>" + nomCocktail + "</h1>";
+        let contenuDeMaDiv = "<div>";
+        contenuDeMaDiv = contenuDeMaDiv+ "<h2>" + nomCocktail + "</h2>";
         contenuDeMaDiv =
           contenuDeMaDiv + `<img src="${cocktail.strDrinkThumb}" />`;
+        contenuDeMaDiv = contenuDeMaDiv + "</div>";
+        contenuDeMaDiv = contenuDeMaDiv + "<div class='containerInformation'>";
 
+        contenuDeMaDiv = contenuDeMaDiv + "<div>";
         for (let i = 0; i < ingredientsCocktail.length; i++) {
           contenuDeMaDiv =
             contenuDeMaDiv + "<p>" + ingredientsCocktail[i] + "</p>";
         }
+        contenuDeMaDiv = contenuDeMaDiv + "</div>";
 
+        contenuDeMaDiv = contenuDeMaDiv + "<div>";
         for (let i = 0; i < mesurecocktail.length; i++) {
           contenuDeMaDiv = contenuDeMaDiv + "<p>" + mesurecocktail[i] + "</p>";
         }
+        contenuDeMaDiv = contenuDeMaDiv + "<div>";
+        contenuDeMaDiv = contenuDeMaDiv + "</div>";
 
         alcoolDiv.innerHTML = contenuDeMaDiv;
         document.getElementById("cocktail").appendChild(alcoolDiv);
       });
     });
 }
-
-
-//dans index.html, dans la ballise boutton rajouter une propriété onClick ="fonction fléclée" (celle dans script.js)
+//dans index.html, dans la balise boutton rajouter une propriété onClick ="fonction fléclée" (celle dans script.js)
 // onclick="() => toto(alcool en fonction du boutton)"
